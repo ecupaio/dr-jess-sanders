39873c4e1d9d6e62c3d0cfe758a63a0edefb4e52
ecupaio <ed.cupaioli@arcadiapower.com>
2018-10-24 16:44:36 -0400

    added sticky toggle

--- js/site_functions.js
+++ js/site_functions.js
@@ -3,5 +3,14 @@ $(function(){
   $('.menu-toggle').click(function() {
     $('#site-nav, #main').toggleClass('menu-open');
   });
-
+  var siteNav = $('#site-nav').offset().top;
+  $(window).scroll(function() {
+    var top = $(window).scrollTop();
+      if ($(window).scrollTop() > siteNav) {
+          $('#site-nav').css('top',top);
+      }
+      else {
+        $('#site-nav').css('top','1em');
+      }
+  });
 });
