8619a1f04e95fb1c9861119f8bb737fe15472c59
Ed Cupaioli <ecupaio@gmail.com>
2021-08-02 15:16:52 -0700

    added leaves bg

--- gruntfile.js
+++ gruntfile.js
@@ -40,9 +40,6 @@ module.exports = function(grunt) {
       }
     },
     shell: {
-      installBundle: {
-        command: 'bundle'
-      },
       jekyllServe: {
         command: 'bundle exec jekyll serve --livereload '
       }
@@ -60,5 +57,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-shell');
-  grunt.registerTask('default', ['shell:installBundle','concurrent:target']);
+  grunt.registerTask('default', ['concurrent:target']);
 };
